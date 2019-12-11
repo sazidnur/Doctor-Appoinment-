8ddf6048b04ad3e3f755d066aec78cf09e4306d3
Mamun-swe <mhmamun166009@gmail.com>
2019-12-11 10:06:55 +0600

    OK

--- src/main.js
+++ src/main.js
@@ -9,10 +9,10 @@ import VueSimpleAlert from "vue-simple-alert";
 
 
 Vue.prototype.$axios = axios
-Vue.prototype.$admin_api = "http://localhost:3000/api/admin/"
-Vue.prototype.$doctor_api = "http://localhost:3000/api/doctor/"
-Vue.prototype.$patient_api = "http://localhost:3000/api/patient/"
-Vue.prototype.$login_api = "http://localhost:3000/api/login/"
+Vue.prototype.$admin_api = "https://maternal-api.herokuapp.com//api/admin/"
+Vue.prototype.$doctor_api = "https://maternal-api.herokuapp.com//api/doctor/"
+Vue.prototype.$patient_api = "https://maternal-api.herokuapp.com//api/patient/"
+Vue.prototype.$login_api = "https://maternal-api.herokuapp.com//api/login/"
 Vue.use(VueSimpleAlert, { reverseButtons: true });
 Vue.config.productionTip = false
 
